665e8c05f86de467258256d8d3b25c2e9a5fca42
Adam <electronicbattleweapon7@gmail.com>
2014-06-04 15:16:46 -0500

    Create a `db.Query`, defer closing rows, itterate over `Rows` calling `Scan`, read results into variables, print vars.
    Don't forget to create the `users` table first and give it some users!

--- main.go
+++ main.go
@@ -8,16 +8,33 @@ import (
 )
 
 func main() {
+	var (
+		id   int
+		name string
+	)
+
 	db, err := sql.Open("mysql", "root:@tcp(127.0.0.1:3306)/go-db-example")
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer db.Close()
 
-	err = db.Ping()
+	rows, err := db.Query("select id, name from users where id = ?", 1)
 	if err != nil {
 		log.Fatal(err)
-	} else {
-		log.Print("pong")
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		err := rows.Scan(&id, &name)
+		if err != nil {
+			log.Fatal(err)
+		}
+		log.Println(id, name)
+	}
+
+	err = rows.Err()
+	if err != nil {
+		log.Fatal(err)
 	}
 }
