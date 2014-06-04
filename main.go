660ba52f6dbb590fdfaf3d794cd980573c12e506
Adam <electronicbattleweapon7@gmail.com>
2014-06-04 14:49:50 -0500

    Create a new DB connection and Ping it
    Don't forget to `go get` the mysql driver
    
    Creates a db connection (don't forget to create the db first), calls
    `Ping` and logs "pong" if there are no errors.

---
+++ main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"database/sql"
+	"log"
+
+	_ "github.com/go-sql-driver/mysql"
+)
+
+func main() {
+	db, err := sql.Open("mysql", "root:@tcp(127.0.0.1:3306)/go-db-example")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	err = db.Ping()
+	if err != nil {
+		log.Fatal(err)
+	} else {
+		log.Print("pong")
+	}
+}
